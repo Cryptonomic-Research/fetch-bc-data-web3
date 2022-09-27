460405fadbb35254ac26518137264b6f1201ec30
lemonsuy <lemon.pattharathon@gmail.com>
2022-09-27 14:48:09 +0700

    Add query tx data

--- src/fetcher.js
+++ src/fetcher.js
@@ -6,4 +6,22 @@ var web3 = new Web3(
   )
 );
 
-web3.eth.getBlock(10000000).then(console.log);
+// Get block data from block number
+const getBlockData = async (blockNo) => {
+  return web3.eth.getBlock(blockNo);
+};
+
+// Get transaction data from tx hash
+const getTransactionData = async (txHash) => {
+  return web3.eth.getTransaction(txHash);
+};
+
+(async () => {
+  var block = await getBlockData(10000000);
+  console.log(block.transactions);
+  block.transactions.map(async (txHash, i) => {
+    var tx = await getTransactionData(txHash);
+    console.log(`=======================[Tx${i}]=======================`);
+    console.log(tx);
+  });
+})();
